16bc997a7c8186d38a92c5a54f516c71fc680ff9
ChamroeunProMaxQ <stevenkakoreal@gmail.com>
2023-01-06 14:27:37 +0700

    modify build

--- kunapheap-web/src/app/api/apiRoute.js
+++ kunapheap-web/src/app/api/apiRoute.js
@@ -1,10 +1,10 @@
-const url = 'http://localhost:8080'
+// const url = 'http://localhost:8080'
 // const url = 'http://192.168.8.137:8080'
 // const url = 'http://192.168.127.160:8080'
 // const url = 'http://192.168.144.160:8080'
 // const url = 'http://172.20.10.7:8080'
 // const url = 'http://kunapheap.com:8000'
-// const url = 'https://kunapheap.com'
+const url = 'https://kunapheap.com'
 
 const  api = {
     'user_login_url' : url + '/user/login',
